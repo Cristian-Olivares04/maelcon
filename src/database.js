a95e69736bce97af6c5b904ef84eeda201cfba3c
andres <60414032+sandres132@users.noreply.github.com>
2022-03-02 03:30:38 -0600

    Creacion del proyexto en angular
    Es el primer commit donde se creo el proyecto base de angular

--- src/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-require("dotenv").config({path:'./variables.env'});
- 
-//process.env.
-mongoose
-  .connect(process.env.DB_URL)
-  .then((db) => console.log("Connected to Mongo"))
-  .catch((err) => console.log("Error connecting to Mongo: ", err));
