9eb6b102c572246fd3a972c2f59eb2688ab3b404
Cristian-Olivares04 <102553462+Cristian-Olivares04@users.noreply.github.com>
2022-04-15 22:23:19 -0600

    Actualizando duración del token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const singInSQL = async (req, res) => {
         .status(401)
         .json({ mensaje: "Contrasena SQL erronea", token: null });
     const tokenSQL = jwt.sign({ id: user[0].ID_USUARIO }, config.SECRET, {
-      expiresIn: 120,
+      expiresIn: 84600,
     });
 
     res.json({ token: tokenSQL, user: user });
