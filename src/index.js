1c54563e29871ac682f847ded78c207668c74aeb
Nelson Rivera <isaacriverahn7@gmail.com>
2022-04-19 19:05:26 -0600

    Manejo de errores de socket

--- src/index.js
+++ src/index.js
@@ -9,3 +9,8 @@ const port = process.env.PORT || 3000;
 app.listen(port, host, () =>
   console.log("Ejecutando el server en el puerto ", port)
 );
+
+app.on("clientError", (err, socket) => {
+  console.log(err);
+  socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
+});
